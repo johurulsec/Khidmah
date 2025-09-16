4896c338eda210cbd9697ed914f2e506db7ac7b2
johurulsec <johircse37@gmail.com>
2025-09-16 17:37:04 +0600

    latest full ecommerce project using django aws postgreSql and cart all features

--- carts/views.py
+++ carts/views.py
@@ -65,6 +65,7 @@ def add_cart(request, product_id):
                 cart_item.variations.clear()
                 cart_item.variations.add(*product_variation)
             cart_item.save()
+        
         return redirect('cart')
     # If the user is not authenticated
     else:
