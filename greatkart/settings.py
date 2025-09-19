f73c31edb636b1bfc87a33a69807d199592e5273
johurulsec <johircse37@gmail.com>
2025-09-19 15:26:46 +0600

    logo and cover img updated

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -22,7 +22,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
